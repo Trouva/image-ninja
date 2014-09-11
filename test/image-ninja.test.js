3f186b7fb8d5cd2af5469836370d3c8babe0026b
Vadim Demedes <vdemedes@gmail.com>
2014-09-11 17:58:40 +0300

    presets

--- test/image-ninja.test.js
+++ test/image-ninja.test.js
@@ -143,6 +143,71 @@ describe ('ImageNinja', function () {
         });
     });
 
+    describe ('Presets', function () {
+        var mobilePreset;
+
+        it ('should define a preset', function () {
+            mobilePreset = Image.preset('mobile');
+            mobilePreset.width(500)
+                        .height(350)
+                        .force()
+                        .crop(5, 5, 100, 100)
+                        .format('png');
+
+            mobilePreset.config.width.should.equal(500);
+            mobilePreset.config.height.should.equal(350);
+            mobilePreset.config.force.should.equal(true);
+            mobilePreset.config.crop[0].should.equal(5);
+            mobilePreset.config.crop[1].should.equal(5);
+            mobilePreset.config.crop[2].should.equal(100);
+            mobilePreset.config.crop[3].should.equal(100);
+            mobilePreset.config.format.should.equal('png');
+
+            mobilePreset.args['-resize'].should.equal('500x350!');
+            mobilePreset.args['-crop'].should.equal('100x100+5+5');
+
+            mobilePreset.resize(200, 250);
+
+            mobilePreset.config.width.should.equal(200);
+            mobilePreset.config.height.should.equal(250);
+            mobilePreset.config.force.should.equal(true);
+
+            mobilePreset.args['-resize'].should.equal('200x250!');
+            mobilePreset.args['-crop'].should.equal('100x100+5+5');
+        });
+
+        it ('should use a preset', function () {
+            var image = new Image(testImage);
+            image.width(300)
+                .height(300);
+
+            image.config.width.should.equal(300);
+            image.config.height.should.equal(300);
+
+            image.args['-resize'].should.equal('300x300');
+
+            image.use(mobilePreset);
+
+            image.config.width.should.equal(200);
+            image.config.height.should.equal(250);
+            image.config.force.should.equal(true);
+            image.config.crop[0].should.equal(5);
+            image.config.crop[1].should.equal(5);
+            image.config.crop[2].should.equal(100);
+            image.config.crop[3].should.equal(100);
+            image.config.format.should.equal('png');
+
+            image.args['-resize'].should.equal('200x250!');
+            image.args['-crop'].should.equal('100x100+5+5');
+        });
+
+        it ('should save a preset', function () {
+            mobilePreset.save();
+
+            should.exist(Image.presets['mobile']);
+        });
+    });
+
     after(function (done) {
         exec('rm -f /tmp/*.png /tmp/*.jpg', function () {
             done();
